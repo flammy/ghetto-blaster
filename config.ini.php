c0bd92cf789dbccb19cfadd0fa698fc41565473d
Lukas Rademacher <lukas@rademacher.ac>
2015-08-27 17:21:59 +0200

    .gitignore
    ignore logfile

--- config.ini.php
+++ config.ini.php
@@ -14,9 +14,20 @@ logActions = true
 
 ; paths to site files
 [system]
+; choose your runtime variables, these are important for linux
 env = "XDG_RUNTIME_DIR=/run/user/1000 TERM=xterm"
+
+; choose your player  (mac = afplay, linux = mplayer)
 player = "mplayer"
+
+; choose your tts  (mac = say, linux = espeak)
 tts = "espeak"
+
+
+;if you set debug to "" you are able to debug the return value from your player / tts / killall 
+; warning, if you change this, the command blocks the apacke process
+; debug = ""
+; debug disabled:
 debug = "> /dev/null 2>&1 &"
 
 [paths]
@@ -30,8 +41,6 @@ sfx = "/home/monitor/Musik/ghetto-blaster/"
 ; format to use: 
 ;username = "password"
 
-hello = "world"
-pete = "ginger"
 
 ;*/
 ;?>
